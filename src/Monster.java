06442d20c96da20c6663ce1f37cead3280b3a84c
Thomas Hocking <thomashocking@Thomass-MacBook-Air.local>
2012-09-20 14:01:05 -0500

    mmmmmmmmm updating

---
+++ src/Monster.java
@@ -0,0 +1,48 @@
+
+public abstract class Monster {
+	private String Name;
+	private int attack;
+	private int health;
+	private int goldDrop;
+	private int expDrop;
+	
+	public void setName(String name){
+		this.Name = name;
+	}
+	
+	public String getName(){
+		return this.Name;
+	}
+	
+	public void setAttack(int attack){
+		this.attack = attack;
+	}
+	
+	public int getAttack(){
+		return this.attack;
+	}
+	
+	public void setHealth(int health){
+		this.health = health;
+	}
+	
+	public int getHealth(){
+		return this.health;
+	}
+	
+	public void setGoldDrop(int gold){
+		this.goldDrop = gold;
+	}
+	
+	public int getGoldDrop(){
+		return this.goldDrop;
+	}
+	
+	public void setExpDrop(int exp){
+		this.expDrop = exp;
+	}
+	
+	public int getExpDrop(){
+		return this.expDrop;
+	}
+}
